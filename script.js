569d9eff0b556b95bed604ba2c5f37da961d7ead
fedonch <fedonch@gmail.com>
2024-05-28 15:53:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,6 @@
         let responseData;
         let callbackURL;
 
-        fetch('https://ls-customerserver.onrender.com/swagger/customerOrders')
-            .then(response => response.json())
-            .then(data => {
-                    console.log("order_data_new");
-                    console.log(data);
-                })
-            .catch(error => console.error('Error fetching data:', error));
-
         // Fetch data from server and populate the form
         fetch('https://ls-customerserver.onrender.com/latest')
             .then(response => response.json())
@@ -21,7 +13,7 @@
                 let birthdate = data.birthMonth + "/" +data.birthDay+ "/"+ data.birthYear;
                 let gender = data.gender;
                 let customerAddressInfo =data.postalAddresses[0];
-                let finalAddress = customerAddressInfo.addressConsent + " " +customerAddressInfo.addressLine1 +" "+customerAddressInfo.city +" "+customerAddressInfo.postalCode; 
+                let finalAddress = data.postalAddresses[0].addressConsent + " " +data.postalAddresses[0].addressLine1 +" "+data.postalAddresses[0].city +" "+data.postalAddresses[0].postalCode; 
                 let phone = data.phones[0].phone;
                 let phonetype = data.phones[0].phoneType;
                 console.log(birthdate , gender ,finalAddress,phone,phonetype);
@@ -41,15 +33,35 @@
         document.getElementById('saveChangesBtn').addEventListener('click', function() {
             // Get the current values from the form
             var customerId = document.getElementById('customerId').value;
-            var name = document.getElementById('name').value;
+            var firstName = document.getElementById('name').value;
             var email = document.getElementById('email').value;
             var lastName = document.getElementById('lastName').value;
+            var birthdate = document.getElementById('birthdate').value;
+            var gender = document.getElementById('gender').value;
+            var phone = document.getElementById('phone').value;
+            var phonetype = document.getElementById('phonetype').value;
+            var address = document.getElementById('address').value;
 
             // Update the data
             responseData.customerId = customerId;
-            responseData.firstName = name;
-            responseData.emails[0].email = email;
+            responseData.firstName = firstName;
             responseData.lastName = lastName;
+            responseData.emails[0].email = email;
+            responseData.gender=gender;
+            responseData.phones[0].phone = phone;
+            responseData.phones[0].phoneType = phonetype;
+            
+            const birthDates = birthdate.split("/");
+
+            responseData.birthMonth =birthDates[0];
+            responseData.birthDay=birthDates[1];
+            responseData.birthYear=birthDates[2];
+            
+            const addresses = address.split(" ");
+            responseData.postalAddresses[0].addressConsent=addresses[0];
+            responseData.postalAddresses[0].addressLine1=addresses[1];
+            responseData.postalAddresses[0].city =addresses[2];
+            responseData.postalAddresses[0].postalCode=addresses[3];
 
             // Log the updated response to the console
             console.log('Updated response:', responseData);
