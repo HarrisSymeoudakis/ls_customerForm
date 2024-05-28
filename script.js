153ba915be6c9800ce99811bdf91761b895dd1a9
fedonch <fedonch@gmail.com>
2024-05-28 14:23:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,59 @@
-// Declare response variable using let
-let responseData;
-let callbackURL;
 
-// Fetch data from server and populate the form
-fetch('https://ls-customerserver.onrender.com/latest')
-    .then(response => response.json())
-    .then(data => {
-        callbackURL = data.callbackUrl;
-        delete data.callbackUrl;
-        responseData = data;
-        document.getElementById('customerId').value = data.customerId;
-        document.getElementById('firstName').value = data.firstName; // Changed 'name' to 'firstName' to match your HTML
-        document.getElementById('lastName').value = data.lastName;
-        document.getElementById('email').value = data.emails[0].email;
-    })
-    .catch(error => console.error('Error fetching data:', error));
+        // Declare response variable using let
+        let responseData;
+        let callbackURL;
 
-// Add event listener to the "Save changes" button
-document.getElementById('saveChangesBtn').addEventListener('click', function() {
-    // Get the current values from the form
-    var customerId = document.getElementById('customerId').value;
-    var firstName = document.getElementById('firstName').value; // Changed 'name' to 'firstName' to match your HTML
-    var lastName = document.getElementById('lastName').value;
-    var email = document.getElementById('email').value;
+        // Fetch data from server and populate the form
+        fetch('https://ls-customerserver.onrender.com/latest')
+            .then(response => response.json())
+            .then(data => {
+                callbackURL = data.callbackUrl;
+                delete data.callbackUrl;
+                responseData = data;
+                let birthdate = data.birthMonth + "//" +data.birthDay+ "//"+ data.birthYear;
+                let gender = data.gender;
+                let customerAddressInfo =data.postaladdresses[0];
+                let finalAddress = customerAddressInfo.addressConsent + "" +customerAddressInfo.addressLine1 +""+customerAddressInfo.city +""+customerAddressInfo.postalCode; 
+                let phone = data.phones[0].phone;
+                let phonetype = data.phones[0].phoneType;
+                console.log(birthdate , gender ,finalAddress,phone,phonetype);
+                document.getElementById('customerId').value = data.customerId;
+                document.getElementById('name').value = data.firstName;
+                document.getElementById('lastName').value = data.lastName;
+                document.getElementById('email').value = data.emails[0].email; // Assuming you have an input field with id 'email' for email
+            })
+            .catch(error => console.error('Error fetching data:', error));
 
-    // Update the data
-    responseData.customerId = customerId;
-    responseData.firstName = firstName;
-    responseData.lastName = lastName;
-    responseData.emails[0].email = email;
+        // Add event listener to the "Save changes" button
+        document.getElementById('saveChangesBtn').addEventListener('click', function() {
+            // Get the current values from the form
+            var customerId = document.getElementById('customerId').value;
+            var name = document.getElementById('name').value;
+            var email = document.getElementById('email').value;
+            var lastName = document.getElementById('lastName').value;
 
-    // Log the updated response to the console
-    console.log('Updated response:', responseData);
+            // Update the data
+            responseData.customerId = customerId;
+            responseData.firstName = name;
+            responseData.emails[0].email = email;
+            responseData.lastName = lastName;
 
-    // If you need to send this data to a server, you would use a method like fetch or XMLHttpRequest here
-    console.log('callbackURL:', callbackURL);
-    fetch('https://ls-customerserver.onrender.com/returning', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(responseData)
-    }).then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-    }).catch((error) => {
-        console.error('Error:', error);
-    });
-});
+            // Log the updated response to the console
+            console.log('Updated response:', responseData);
+
+            // If you need to send this data to a server, you would use a method like fetch or XMLHttpRequest here
+            console.log('callbackURL:', callbackURL);
+            fetch('https://ls-customerserver.onrender.com/returning', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(responseData)
+            }).then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+            }).catch((error) => {
+                console.error('Error:', error);
+            });
+        });
+   
