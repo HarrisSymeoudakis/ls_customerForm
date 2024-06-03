8002d4af237e842724cbc29a686a86b70a9530ad
Harris Symeoudakis <harris.symeoudakis@gmail.com>
2024-06-03 09:58:06 +0300

    Update postBackExternalCusForm.js

--- postBackExternalCusForm.js
+++ postBackExternalCusForm.js
@@ -7,6 +7,10 @@ document.getElementById('saveChangesBtn').addEventListener('click', function() {
 
 	 // Define the endpoint for your Node.js server
     const nodeJsEndpoint = 'https://ls-customerserver.onrender.com/api/PostBackExtForm';
+	
+	// Get customerID
+	const cusIdtextField = document.getElementById('customerId');
+    const customerId = cusIdtextField.value;
 
     // Make the POST request to your Node.js server
     fetch(nodeJsEndpoint, {
@@ -19,7 +23,8 @@ document.getElementById('saveChangesBtn').addEventListener('click', function() {
     .then(response => response.json())
     .then(data => {
         console.log('Success:', data);
-        window.location.href = `https://ls-customerserver.onrender.com/PostBackExtForm/redirect?sessionId=${sessionId}`;
+        //window.location.href = `https://ls-customerserver.onrender.com/PostBackExtForm/redirect?sessionId=${sessionId}`;
+        window.location.href = `https://retail-services.cegid.cloud/et/pos/assignCustomer:${customerId}`;
     })
     .catch(error => {
         console.error('Error:', error);
