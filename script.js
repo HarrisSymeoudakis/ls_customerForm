7d41c5518ea49baa5214534788559d8d45344c18
fedonch <fedonch@gmail.com>
2024-06-13 14:43:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -226,7 +226,7 @@ function showEditablePopup(orderIndex) {
 
 				const customerId=order.header.customer.id;
 				const storeId=order.header.storeId;
-				viewBaskerAll(customerId,storeId);
+				viewBasket(customerId,storeId);
 				// document.getElementById('viewBasketAll').addEventListener('click', function() {
 
 			});
@@ -237,8 +237,8 @@ function showEditablePopup(orderIndex) {
 
 function viewBasket(customerId,storeId){
     var xhr = new XMLHttpRequest();
-    var postUrl = 'http://localhost:3000/et/pos/external-basket/v1'; // Proxy server URL
-    //var postUrl = 'https://proxyserver-z74x.onrender.com/et/pos/external-basket/v1'; // Proxy server URL
+    // var postUrl = 'http://localhost:3000/et/pos/external-basket/v1'; // Proxy server URL
+    var postUrl = 'https://proxyserver-z74x.onrender.com/et/pos/external-basket/v1'; // Proxy server URL
     xhr.open('POST', postUrl, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken); // Include access token in the request headers
