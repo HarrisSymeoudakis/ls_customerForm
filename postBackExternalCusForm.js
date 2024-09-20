ddc7eafd81ceb129b93acc3fed044002f4be164a
fedonch <fedonch@gmail.com>
2024-09-20 14:45:02 +0300

    Update postBackExternalCusForm.js

--- postBackExternalCusForm.js
+++ postBackExternalCusForm.js
@@ -1,36 +1,36 @@
-document.getElementById('saveChangesBtn').addEventListener('click', function() {
-	// Get the full URL
-	const url = new URL(window.location.href);
+// document.getElementById('saveChangesBtn').addEventListener('click', function() {
+// 	// Get the full URL
+// 	const url = new URL(window.location.href);
 
-	// Get the sessionId from the query parameters
-	const sessionId = url.searchParams.get('sessionId');
+// 	// Get the sessionId from the query parameters
+// 	const sessionId = url.searchParams.get('sessionId');
 
-	 // Define the endpoint for your Node.js server
-    const nodeJsEndpoint = 'https://ls-customerserver.onrender.com/api/PostBackExtForm';
+// 	 // Define the endpoint for your Node.js server
+//     const nodeJsEndpoint = 'https://ls-customerserver.onrender.com/api/PostBackExtForm';
 	
-	// Get customerID
-	const cusIdtextField = document.getElementById('customerId');
-    const customerId = cusIdtextField.value;
+// 	// Get customerID
+// 	const cusIdtextField = document.getElementById('customerId');
+//     const customerId = cusIdtextField.value;
 
-    // Make the POST request to your Node.js server
-    fetch(nodeJsEndpoint, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ sessionId: sessionId })
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-        //window.location.href = `https://ls-customerserver.onrender.com/PostBackExtForm/redirect?sessionId=${sessionId}`;
-        window.location.href = `https://retail-services.cegid.cloud/et/pos/assignCustomer/${customerId}`;
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-});
+//     // Make the POST request to your Node.js server
+//     fetch(nodeJsEndpoint, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({ sessionId: sessionId })
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log('Success:', data);
+//         //window.location.href = `https://ls-customerserver.onrender.com/PostBackExtForm/redirect?sessionId=${sessionId}`;
+//         window.location.href = `https://retail-services.cegid.cloud/et/pos/assignCustomer/${customerId}`;
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//     });
+// });
 
-document.getElementById('cancelBtn').addEventListener('click', function() {
-	window.location.href = 'https://retail-services.cegid.cloud/et/pos/';
-});
+// document.getElementById('cancelBtn').addEventListener('click', function() {
+// 	window.location.href = 'https://retail-services.cegid.cloud/et/pos/';
+// });
